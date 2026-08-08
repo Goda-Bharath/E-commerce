4b3b74690da5a570024bd48f55a5975f97185974
Bharath-goda87 <godabharath2004@gmail.com>
2026-08-08 09:14:41 +0530

    Update footer design and styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,61 @@
 function Footer() {
-    return(
-    
-      <div className="bg-gray-900 py-24 sm:py-32 z-50 bg-gradient-to-r from-indigo-900 via-purple-900 to-cyan-700/80 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-cyan-400/40 shadow-lg"  id="contact">
+  return (
+    // Main footer section
+    <div
+      id="contact"
+      className="relative z-50 bg-gradient-to-r from-indigo-900 via-purple-900 to-cyan-700/80 py-24 shadow-lg sm:py-32"
+    >
+      {/* Footer container */}
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg/8 font-semibold text-white">Trusted by the world’s most innovative brand</h2>
+
+        {/* Footer heading */}
+        <h2 className="text-center text-lg font-semibold leading-8 text-white">
+          Trusted by the world’s most innovative brands
+        </h2>
+
+        {/* Brand logos grid */}
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+
+          {/* Transistor logo */}
           <img
-            alt="Transistor"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-white.svg"
+            alt="Transistor"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
 
+          {/* Reform logo */}
           <img
-            alt="Reform"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-white.svg"
+            alt="Reform"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
 
+          {/* Tuple logo */}
           <img
-            alt="Tuple"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-white.svg"
+            alt="Tuple"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
           />
 
+          {/* SavvyCal logo */}
           <img
-            alt="SavvyCal"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-white.svg"
+            alt="SavvyCal"
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
           />
 
+          {/* Statamic logo */}
           <img
-            alt="Statamic"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-white.svg"
+            alt="Statamic"
             width={158}
             height={48}
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
@@ -47,8 +63,7 @@ function Footer() {
         </div>
       </div>
     </div>
-    
-  )
+  );
 }
 
 export default Footer;
